0b9bffc49357618ba89845f26ebb76c888b473e0
João Lemes <joao.lemes.info@gmail.com>
2025-03-07 18:28:47 -0300

    fix: Validate email is unique

--- Modules/User/app/Http/Requests/StoreUserRequest.php
+++ Modules/User/app/Http/Requests/StoreUserRequest.php
@@ -16,7 +16,7 @@ class StoreUserRequest extends BaseRequest
     {
         return [
             'name' => 'required|string|max:255',
-            'email' => 'required|email|max:255',
+            'email' => 'required|email|max:255|unique:users',
             'password' => 'required|string|min:8',
             'password_confirmation' => 'required|string|same:password',
         ];
